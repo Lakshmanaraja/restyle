7ef8fe2930b97b2296ad6315a0e7d9d1a1ee6c46
Lakshmanaraja kannan <96323209+Lakshmanaraja@users.noreply.github.com>
2022-09-24 20:32:32 +0800

    Update paths_config.py

--- configs/paths_config.py
+++ configs/paths_config.py
@@ -15,7 +15,7 @@ dataset_paths = {
 	'afhq_wild_test': '' ,
 	
 	
-  	'style_human_train': '/content/data/SHHQ-1.0_samples' ,
+  	'style_human_train': '/content/data/train' ,
  	'style_human_test': '/content/data/test'
 }
 

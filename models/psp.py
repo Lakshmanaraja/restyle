3fbe18026d974dd9a0101d2ef9a71d0b42d1f53f
Lakshmanaraja kannan <96323209+Lakshmanaraja@users.noreply.github.com>
2022-10-07 14:39:07 +0800

    Update psp.py

--- models/psp.py
+++ models/psp.py
@@ -8,7 +8,7 @@ from torch import nn
 from models.stylegan2.model import Generator
 from configs.paths_config import model_paths
 from models.encoders import fpn_encoders, restyle_psp_encoders
-from utils.model_utils import RESNET_MAPPING
+from restyle.utils.model_utils import RESNET_MAPPING
 
 
 class pSp(nn.Module):
